f17bf2d6e3c69485b9554e78af30e0f1992a46cc
Joy Wang <108701016+joyqvq@users.noreply.github.com>
2024-02-06 20:33:01 -0500

    fix typo

--- src/example1.rs
+++ src/example1.rs
@@ -49,7 +49,7 @@ impl<F: FieldExt> FibonacciChip<F> {
             let a = meta.query_advice(col_a, Rotation::cur());
             let b = meta.query_advice(col_b, Rotation::cur());
             let c = meta.query_advice(col_c, Rotation::cur());
-            vec![s * (a + b) - c]
+            vec![s * (a + b - c)]
         });
         FibonacciConfig {
             col_a,
